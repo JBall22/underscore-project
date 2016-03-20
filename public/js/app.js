9c7886edc2c0a165a74dd13055cbd30525d62173
Justin Ball <jball1622@gmail.com>
2016-03-20 19:14:32 -0400

    Last

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,47 @@
-// First Object
+// First
 var firstObj = _.first(products);
 
 _.each(firstObj, function(value, key, list) {
   if (key === 'name') {
-    $('#featured').html('<div id="book1"><h1>' + list[key] + '</h1></div>');
+    $('#featured').html('<div id="first-item"><h1>' + list[key] + '</h1></div>');
   } else if (key === 'category') {
-    $('#book1').addClass(list[key]);
+    $('#first-item').addClass(list[key]);
   } else if (key === 'selling_points') {
-    $('#book1').append('<ul></ul>');
+    $('#first-item').append('<ul></ul>');
     var sellPoints = firstObj['selling_points'];
     _.each(sellPoints, function(element, index, list) {
-        $('#book1 > ul').append('<li class="selling-points">' + list[index] + '</li>');
+        $('#first-item > ul').append('<li class="selling-points">' + list[index] + '</li>');
     });
   } else if (key === 'price') {
     var price = firstObj['price'];
-    $('#book1').append('<h3 class="price">Price: $' + price + '</h3>');
+    $('#first-item').append('<h3 class="price">Price: $' + price + '</h3>');
   } else if (key === 'picture_url') {
     var pic = firstObj['picture_url'];
-    $('#book1').append('<img class="image">');
-    $('#book1 > img').attr('src', '' + pic + '');
+    $('#first-item').append('<img class="image">');
+    $('#first-item > img').attr('src', '' + pic + '');
   }
 });
 
+// Last
+var lastObj = _.last(products);
+
+_.each(lastObj, function(value, key, list) {
+  if (key === 'name') {
+    $('#clearance').html('<div id="last-item"><h1>' + list[key] + '</h1></div>');
+  } else if (key === 'category') {
+    $('#last-item').addClass(list[key]);
+  } else if (key === 'selling_points') {
+    $('#last-item').append('<ul></ul>');
+    var sellPoints = lastObj['selling_points'];
+    _.each(sellPoints, function(element, index, list) {
+        $('#last-item > ul').append('<li class="selling-points">' + list[index] + '</li>');
+    });
+  } else if (key === 'price') {
+    var price = lastObj['price'];
+    $('#last-item').append('<h3 class="price">Price: $' + price + '</h3>');
+  } else if (key === 'picture_url') {
+    var pic = lastObj['picture_url'];
+    $('#last-item').append('<img class="image">');
+    $('#last-item > img').attr('src', '' + pic + '');
+  }
+});
